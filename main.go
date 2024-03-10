8ea9b2e2e4fb79a24e01d916c53616d0fe994389
kwa0x2 <alperenibo14@gmail.com>
2024-03-10 18:45:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
 	"github.com/gin-contrib/cors"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"nettasec.co.uk/landing/backend/config"
 	"nettasec.co.uk/landing/backend/routes"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	allowedIPs := os.Getenv("ALLOWED_IPS")
-	allowedIPList := strings.Split(allowedIPs, ",")
-	fmt.Print("ips", allowedIPList)
 
 	router := gin.New()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     allowedIPList,
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		AllowCredentials: true,
